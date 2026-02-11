040149eaca93f56e316f4b4b7f436d6e78cc1fdd
Minato1123 <slamdunk0522@gmail.com>
2026-02-11 09:45:14 +0800

    build: add deploy command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
+  app: {
+    baseURL: '/character-sorter/',
+  },
 })
